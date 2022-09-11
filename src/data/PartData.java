01581939b7d0fcf822388ab74ed0f97a23581fa7
Chromadesk <106117942+Chromadesk@users.noreply.github.com>
2022-09-10 22:40:52 -0400

    Formatting the CSV into arrays, loadPartData() has errors.

--- src/data/PartData.java
+++ src/data/PartData.java
@@ -1,4 +1,5 @@
 package data;
+import models.items.bulletparts.*;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -6,10 +7,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PartData {
 
@@ -17,7 +15,14 @@ public class PartData {
 
     private static final String DATA_FILE = "src/resources/part_data.csv";
 
-    public static ArrayList<Map> allParts;
+    public static ArrayList<Map<String, String>> allParts;
+
+    public static final HashMap<String, Casing> allCasings = new HashMap<>();
+    public static final HashMap<String, Dust> allDust = new HashMap<>();
+    public static final HashMap<String, Powder> allPowder = new HashMap<>();
+    public static final HashMap<String, Primer> allPrimer = new HashMap<>();
+    public static final HashMap<String, Projectile> allProjectile = new HashMap<>();
+    public static final HashMap<String, Shape> allShape = new HashMap<>();
 
     //Copied from techjobs-console-java, modified
     public static void loadData() {
@@ -56,6 +61,67 @@ public class PartData {
         } catch (IOException e) {
             System.out.println("Failed to load part data");
             e.printStackTrace();
+        }
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void loadPartData() {
+        loadData();
+        for (Map<String, String> part : allParts) {
+            HashMap<String, Double> stats = new HashMap<>();
+            stats.put("Damage", 0.0);
+            stats.put("Piercing", 0.0);
+            stats.put("Speed", 0.0);
+            stats.put("Power", 0.0);
+            stats.put("Value", 0.0);
+
+
+            for (String strNum : part.values()) {
+                if(isNumeric(strNum)) {
+                    double num = Double.parseDouble(strNum);
+                    for (Map.Entry<String, Double> stat : stats.entrySet()) {
+                        if(part.containsKey(stat.getKey())) {
+                            stats.replace(stat.getKey(), num);
+                        }
+                    }
+                }
+            }
+
+            if (part.get("type").contains("Casing")) {
+                allCasings.put(part.get("Name"), new Casing(part.get("Name"), stats.get("Damage"), stats.get("Piercing"),
+                        stats.get("Speed"), stats.get("Power"), stats.get("Value")));
+            }
+            if (part.get("type").contains("Dust") || part.get("type").contains("Oil")) {
+                allDust.put(part.get("Name"), new Dust(part.get("Name"), part.get("Type"), stats.get("Damage"), stats.get("Piercing"),
+                        stats.get("Speed"), stats.get("Power"), stats.get("Value")));
+            }
+            if (part.get("type").contains("Powder")) {
+                allPowder.put(part.get("Name"), new Powder(part.get("Name"), stats.get("Damage"), stats.get("Piercing"),
+                        stats.get("Speed"), stats.get("Power"), stats.get("Value")));
+            }
+            if (part.get("type").contains("Primer")) {
+                allPrimer.put(part.get("Name"), new Primer(part.get("Name"), stats.get("Damage"), stats.get("Piercing"),
+                        stats.get("Speed"), stats.get("Power"), stats.get("Value")));
+            }
+            if (part.get("type").contains("Shape")) {
+                allShape.put(part.get("Name"), new Shape(part.get("Name"), stats.get("Damage"), stats.get("Piercing"),
+                        stats.get("Speed"), stats.get("Power"), stats.get("Value")));
+            }
+            if (part.get("type").contains("Solid")) {
+                allProjectile.put(part.get("Name"), new Projectile(part.get("Name"), part.get("Type"), stats.get("Damage"), stats.get("Piercing"),
+                        stats.get("Speed"), stats.get("Power"), stats.get("Value")));
+            }
         }
     }
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,15 @@ import data.PartData;
 
 public class Main {
     public static void main (String[] args) {
-        PartData.loadData();
+        System.out.println("running main");
+        PartData.loadPartData();
+        System.out.println("ooga booga");
         System.out.println(PartData.allParts);
+        System.out.println(PartData.allCasings);
+        System.out.println(PartData.allShape);
+        System.out.println(PartData.allDust);
+        System.out.println(PartData.allPowder);
+        System.out.println(PartData.allPrimer);
+        System.out.println(PartData.allProjectile);
     }
 }
